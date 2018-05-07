339babba9a24e33345cd5eb7bb70b3d27c5ba415
Hitesh Kumar <ihiteshkumarp@gmail.com>
2018-05-07 17:34:46 +0530

    simple code

---
+++ SwiftCoding/SimpleOne/KeepSimpleCodeToRemember.swift
@@ -0,0 +1,9 @@
+//
+//  KeepSimpleCodeToRemember.swift
+//  SwiftCoding
+//
+//  Created by Hitesh Kumar on 07/05/18.
+//  Copyright © 2018 Hitesh Kumar. All rights reserved.
+//
+
+import Foundation
