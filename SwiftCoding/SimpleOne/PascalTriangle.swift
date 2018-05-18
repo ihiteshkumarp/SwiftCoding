cfc81f97f3618a0a732e6c4684a725492783e979
Hitesh Kumar <ihiteshkumarp@gmail.com>
2018-05-18 16:44:58 +0530

    pascal triangle without recursion

--- SwiftCoding/SimpleOne/PascalTriangle.swift
+++ SwiftCoding/SimpleOne/PascalTriangle.swift
@@ -10,7 +10,8 @@
 class Pascal {
     
     func getPascal(){
-        let val = calculateValue(row: 2, col: 3)
+//        let val = calculateValue(row: 2, col: 3)
+        let val = calculateValueWithoutRecursion(row: 2, col: 1)
         print(val)
     }
     
@@ -23,5 +24,39 @@ class Pascal {
         }
         return calculateValue(row: row - 1, col: col - 1) + calculateValue(row: row - 1, col: col)
     }
+    
+    func calculateValueWithoutRecursion(row: Int, col: Int) -> Int {
+        
+        guard row > col else {
+            return -1
+        }
+        
+        var col = col
+        var result = 1
+        // Purpose of this if is:
+        // if you want result for (4, 4), so for this won't have to calculate till 4 becuase
+        // result for the (4, 4) and (4, 0) are the same so we can easily replace col with row - col
+        // Similar for (4, 3), the result will be same as (4, 1)
+        if col > row - col {
+            col = row - col
+        }
+        
+        for i in 0 ..< col {
+            result = result * (row - i)
+            result = result / (i + 1)
+            
+        }
+        return result;
+    }
+    
+    /* Pascan triangle
+     
+    1
+    1 1
+    1 2 1
+    1 3 3 1
+    1 4 6 4 1
+     
+    */
 
 }
